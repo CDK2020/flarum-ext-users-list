c6ddb398cdd7171e5f09c8047d4cee4531b67c5e
Charlie <issyrocks12@users.noreply.github.com>
2017-02-28 22:22:57 -0800

    Html support

--- bootstrap.php
+++ bootstrap.php
@@ -2,8 +2,11 @@
 namespace issyrocks12\UsersList;
 
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\View\Factory;
 
-return function (Dispatcher $events) {
+return function (Dispatcher $events, Factory $views) {
     $events->subscribe(Listener\AddAdminMailApi::class);
     $events->subscribe(Listener\AddClientAssets::class);
+
+    $views->addNameSpace('issyrocks12-userlist', __DIR__.'/views');
 };
